5547ece54e3d955036774f1569aa5d2af394c6ef
Nithin VR <vrnithin123@example.com>
2026-04-24 19:52:49 +0530

    final fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL || "https://bajaj-bhfl-project-sxdi.onrender.com/bfhl";
 
 export default function App() {
   const [input, setInput] = useState("");
